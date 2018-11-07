cb7314b22ba4678ff52ec93c30c39a4599c04545
Ben Scholar <bens@digitalman.com>
2018-11-06 20:26:14 -0800

    some more classes

---
+++ src/main/kotlin/frc/team5499/frc2018Kotlin/subsystems/Intake.kt
@@ -0,0 +1,16 @@
+package frc.team5499.frc2018Kotlin.subsystems
+
+object Intake : Subsystem() {
+
+    override fun update() {
+    }
+
+    override fun stop() {
+    }
+
+    override fun reset() {
+    }
+
+    override fun broadcastToDashboard() {
+    }
+}
