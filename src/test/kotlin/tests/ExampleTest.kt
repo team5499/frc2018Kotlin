532d5c614d8ee7d540b6f033f0a0f65c67a0efd6
Connor Worley <cbworley@yelp.com>
2018-11-08 20:01:18 -0800

    Demonstrate powermock / mockito

--- src/test/kotlin/tests/ExampleTest.kt
+++ src/test/kotlin/tests/ExampleTest.kt
@@ -1,11 +1,26 @@
 package tests
 
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.modules.junit4.PowerMockRunner
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.api.mockito.PowerMockito
+import org.mockito.Mockito
 
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.can.TalonSRX
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(TalonSRX::class)
 public class ExampleTest {
     @Test
     fun test() {
-        println("test")
-        assert(true)
+        var fakeTalon = PowerMockito.mock(TalonSRX::class.java)
+        PowerMockito.whenNew(TalonSRX::class.java).withArguments(1).thenReturn(fakeTalon)
+
+        var talon = TalonSRX(1)
+        talon.set(ControlMode.PercentOutput, 0.5)
+
+        Mockito.verify(fakeTalon).set(ControlMode.PercentOutput, 0.5)
     }
 }
--- src/test/kotlin/tests/utils/Vector2Test.kt
+++ src/test/kotlin/tests/utils/Vector2Test.kt
@@ -1,6 +1,7 @@
 package tests.utils
 
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import org.junit.Assert
 
 import frc.team5499.frc2018Kotlin.utils.Vector2
 
@@ -9,6 +10,39 @@ public class Vector2Test {
     fun test_dot() {
         val a = Vector2(3, 4)
         val b = Vector2(5, 6)
-        assert(a.dot(b) == 39.0)
+        Assert.assertEquals(a.dot(b), 39.0, 1E-5)
+    }
+
+    @Test
+    fun test_magnitude() {
+        val a = Vector2(3, 4)
+        Assert.assertEquals(a.magnitude, 5.0, 1E-5)
+    }
+
+    @Test
+    fun test_distance_between() {
+        val a = Vector2(0, 0)
+        val b = Vector2(3, 4)
+        Assert.assertEquals(a.distanceTo(b), 5.0, 1E-5)
+    }
+
+    @Test
+    fun test_angle() {
+        val a = Vector2(0.5, Math.sqrt(3.0) / 2.0)
+        Assert.assertEquals(a.angle, Math.toRadians(60.0), 1E-5)
+    }
+
+    @Test
+    fun test_normalized() {
+        val a = Vector2(3, 4).normalized
+        Assert.assertEquals(a.x, 3.0 / 5.0, 1E-5)
+        Assert.assertEquals(a.y, 4.0 / 5.0, 1E-5)
+    }
+
+    @Test
+    fun test_zero() {
+        val z = Vector2.Zero
+        Assert.assertEquals(z.x, 0.0, 1E-5)
+        Assert.assertEquals(z.y, 0.0, 1E-5)
     }
 }
