97db23346c38d1461a9a04580e820cacb3c2672d
Ben Scholar <bens@digitalman.com>
2018-11-08 19:07:11 -0800

    added vector 2 tests

--- src/test/kotlin/tests/utils/Vector2Test.kt
+++ src/test/kotlin/tests/utils/Vector2Test.kt
@@ -1,6 +1,7 @@
 package tests.utils
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
 
 import frc.team5499.frc2018Kotlin.utils.Vector2
 
@@ -9,6 +10,39 @@ public class Vector2Test {
     fun test_dot() {
         val a = Vector2(3, 4)
         val b = Vector2(5, 6)
-        assert(a.dot(b) == 39.0)
+        Assertions.assertEquals(a.dot(b), 39.0, 1E-5)
+    }
+
+    @Test
+    fun test_magnitude() {
+        val a = Vector2(3, 4)
+        Assertions.assertEquals(a.magnitude, 5.0, 1E-5)
+    }
+
+    @Test
+    fun test_distance_between() {
+        val a = Vector2(0, 0)
+        val b = Vector2(3, 4)
+        Assertions.assertEquals(a.distanceTo(b), 5.0, 1E-5)
+    }
+
+    @Test
+    fun test_angle() {
+        val a = Vector2(0.5, Math.sqrt(3.0) / 2.0)
+        Assertions.assertEquals(a.angle, Math.toRadians(60.0), 1E-5)
+    }
+
+    @Test
+    fun test_normalized() {
+        val a = Vector2(3, 4).normalized
+        Assertions.assertEquals(a.x, 3.0 / 5.0, 1E-5)
+        Assertions.assertEquals(a.y, 4.0 / 5.0, 1E-5)
+    }
+
+    @Test
+    fun test_zero() {
+        val z = Vector2.Zero
+        Assertions.assertEquals(z.x, 0.0, 1E-5)
+        Assertions.assertEquals(z.y, 0.0, 1E-5)
     }
 }
