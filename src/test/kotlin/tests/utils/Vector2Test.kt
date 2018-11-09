86c2214761eae942003c8767c0c5d6416e5159c5
Connor Worley <cbworley@yelp.com>
2018-11-08 20:01:18 -0800

    Demonstrate powermock / mockito

--- src/test/kotlin/tests/utils/Vector2Test.kt
+++ src/test/kotlin/tests/utils/Vector2Test.kt
@@ -1,7 +1,7 @@
 package tests.utils
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions
+import org.junit.Test
+import org.junit.Assert
 
 import frc.team5499.frc2018Kotlin.utils.Vector2
 
@@ -10,39 +10,39 @@ public class Vector2Test {
     fun test_dot() {
         val a = Vector2(3, 4)
         val b = Vector2(5, 6)
-        Assertions.assertEquals(a.dot(b), 39.0, 1E-5)
+        Assert.assertEquals(a.dot(b), 39.0, 1E-5)
     }
 
     @Test
     fun test_magnitude() {
         val a = Vector2(3, 4)
-        Assertions.assertEquals(a.magnitude, 5.0, 1E-5)
+        Assert.assertEquals(a.magnitude, 5.0, 1E-5)
     }
 
     @Test
     fun test_distance_between() {
         val a = Vector2(0, 0)
         val b = Vector2(3, 4)
-        Assertions.assertEquals(a.distanceTo(b), 5.0, 1E-5)
+        Assert.assertEquals(a.distanceTo(b), 5.0, 1E-5)
     }
 
     @Test
     fun test_angle() {
         val a = Vector2(0.5, Math.sqrt(3.0) / 2.0)
-        Assertions.assertEquals(a.angle, Math.toRadians(60.0), 1E-5)
+        Assert.assertEquals(a.angle, Math.toRadians(60.0), 1E-5)
     }
 
     @Test
     fun test_normalized() {
         val a = Vector2(3, 4).normalized
-        Assertions.assertEquals(a.x, 3.0 / 5.0, 1E-5)
-        Assertions.assertEquals(a.y, 4.0 / 5.0, 1E-5)
+        Assert.assertEquals(a.x, 3.0 / 5.0, 1E-5)
+        Assert.assertEquals(a.y, 4.0 / 5.0, 1E-5)
     }
 
     @Test
     fun test_zero() {
         val z = Vector2.Zero
-        Assertions.assertEquals(z.x, 0.0, 1E-5)
-        Assertions.assertEquals(z.y, 0.0, 1E-5)
+        Assert.assertEquals(z.x, 0.0, 1E-5)
+        Assert.assertEquals(z.y, 0.0, 1E-5)
     }
 }
