ad5b5ba0db8a1d47008713baac3fe23c2284bb9d
Ben Scholar <bens@digitalman.com>
2018-11-17 14:53:14 -0800

    fixed issues with drivetrain

--- src/main/kotlin/frc/team5499/frc2018Kotlin/controllers/AutoController.kt
+++ src/main/kotlin/frc/team5499/frc2018Kotlin/controllers/AutoController.kt
@@ -1,12 +1,8 @@
 package frc.team5499.frc2018Kotlin.controllers
 
-import frc.team5499.frc2018Kotlin.subsystems.Drivetrain
-
 object AutoController : Controller() {
 
     override fun start() {
-        @Suppress("MagicNumber")
-        Drivetrain.setPosition(60.0)
     }
 
     override fun update() {
