b746608b3b0df49950224b02b6e9404b262d00b9
Ben Scholar <bens@digitalman.com>
2018-11-28 18:11:57 -0800

    started on path generator

--- src/main/kotlin/frc/team5499/frc2018Kotlin/path/PathGenerator.kt
+++ src/main/kotlin/frc/team5499/frc2018Kotlin/path/PathGenerator.kt
@@ -1,10 +1,32 @@
 package frc.team5499.frc2018Kotlin.path
 
+import frc.team5499.frc2018Kotlin.utils.math.geometry.Pose2d
+import frc.team5499.frc2018Kotlin.utils.math.geometry.Vector2
+import frc.team5499.frc2018Kotlin.utils.math.geometry.Rotation2d
+
 @SuppressWarnings("MagicNumber")
 object PathGenerator {
 
-    fun generatePath() {
+    val PATHSET by lazy {
+        println("Generating paths...")
+        val startTime = System.currentTimeMillis()
+        PathSet()
+        println("Paths generated in ${System.currentTimeMillis() - startTime}ms")
     }
 
-    object Paths
+    private fun generatePath(
+        reversed: Boolean,
+        waypoints: MutableList<Pose2d>,
+        maxVelo: Double,
+        maxAccel: Double
+    ) {
+    }
+
+    private val startPose = Pose2d(Vector2(0.0, 0.0), Rotation2d.fromDegrees(180.0))
+
+    class PathSet {
+        init {
+            // something
+        }
+    }
 }
--- src/test/kotlin/tests/utils/math/splines/SplineGeneratorTest.kt
+++ src/test/kotlin/tests/utils/math/splines/SplineGeneratorTest.kt
@@ -19,9 +19,9 @@ class SplineGeneratorTest {
     fun test_generator() {
         val p1 = Pose2d(Vector2(0, 0), Rotation2d())
         val p2 = Pose2d(Vector2(15, 10), Rotation2d(1, -5, true))
-        val s = QuinticHermiteSpline(p1, p2)
+        var s = QuinticHermiteSpline(p1, p2)
 
-        val samples: MutableList<Pose2dWithCurvature> = SplineGenerator.parameterizeSpline(s)
+        var samples: MutableList<Pose2dWithCurvature> = SplineGenerator.parameterizeSpline(s)
         var arclength = 0.0
         var curPose = samples.get(0)
         for (pose in samples) {
@@ -29,14 +29,18 @@ class SplineGeneratorTest {
             arclength += t.dx
             curPose = pose
         }
-
-        // for (s in samples) {
-        //     println(s.toString())
-        // }
-
         assertEquals(curPose.pose.translation.x, 15.0, Constants.EPSILON)
         assertEquals(curPose.pose.translation.y, 10.0, Constants.EPSILON)
         assertEquals(curPose.pose.rotation.degrees, -78.69006752597981, Constants.EPSILON)
         assertEquals(arclength, 23.17291953186379, Constants.EPSILON)
+
+        // val p3 = Pose2d(Vector2(100.0, 0.0), Rotation2d())
+        // s = QuinticHermiteSpline(p1, p3)
+        // var t = 0.0
+        // while (t <= 1.0) {
+        //     println("Pos at time $t: ${s.getPoint(t)} ")
+        //     println("Velo at time $t: ${s.getVelocity(t)}")
+        //     t += 0.1
+        // }
     }
 }
