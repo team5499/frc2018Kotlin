3ab867dd8a99443bf69d17678fdd37d668e29ecb
Ben Scholar <bens@digitalman.com>
2018-11-28 18:27:48 -0800

    adjusted drivesignal class

--- src/main/kotlin/frc/team5499/frc2018Kotlin/utils/DriveSignal.kt
+++ src/main/kotlin/frc/team5499/frc2018Kotlin/utils/DriveSignal.kt
@@ -1,7 +1,8 @@
 package frc.team5499.frc2018Kotlin.utils
 
-data class DriveSignal(val left: Double, val right: Double, val brakeMode: Boolean) {
+data class DriveSignal(val left: Double, val right: Double, val brakeMode: Boolean) : CSVWritable {
 
+    constructor(): this(0.0, 0.0)
     constructor(left: Double, right: Double) : this(left, right, false)
 
     companion object {
@@ -12,4 +13,6 @@ data class DriveSignal(val left: Double, val right: Double, val brakeMode: Boole
     override fun toString(): String {
         return "L: $left - R: $right - Brake Mode: $brakeMode "
     }
+
+    override fun toCSV() = "$left,$right,$brakeMode"
 }
