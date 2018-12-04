65d6e1423f4b70eb039f63ad7282417dcf937e11
Ben Scholar <bens@digitalman.com>
2018-12-03 22:12:03 -0800

    added docs for path follower

--- src/main/kotlin/frc/team5499/frc2018Kotlin/path/PathFollower.kt
+++ src/main/kotlin/frc/team5499/frc2018Kotlin/path/PathFollower.kt
@@ -5,6 +5,11 @@ import frc.team5499.frc2018Kotlin.Constants
 import frc.team5499.frc2018Kotlin.utils.math.geometry.Vector2
 import frc.team5499.frc2018Kotlin.utils.math.geometry.Pose2d
 
+/**
+* class that contains methods to follow a list of coordinates
+* used during autonomous
+* @property path the path the follower will follow
+*/
 @SuppressWarnings("MagicNumber")
 class PathFollower(path: Path) {
 
@@ -16,6 +21,12 @@ class PathFollower(path: Path) {
         mLastClosestPointIndex = 0
     }
 
+    /**
+    * Function used by path follower to be run in every periodic tick to calculate
+    * velocities based on current path, robot location, robot angle, and target velocities
+    * @param currentRobotPose the current position and rotation of the drivetrain
+    * @return velocities for the left and right sides of the drivetrain
+    */
     fun update(currentRobotPose: Pose2d): PathFollowerOutput {
         var robotAngle = currentRobotPose.rotation.radians
         if (robotAngle == 0.0) robotAngle = Constants.EPSILON
@@ -43,6 +54,14 @@ class PathFollower(path: Path) {
         return PathFollowerOutput(leftVelo, rightVelo)
     }
 
+    /**
+    *   Calcuates the lookahead point based on robot position and desired lookahead distance.
+    *   Algorithm basically creates a circle around the robot with radius = lookahead distance
+    *   then finds the intersection point with the path line. It then chooses the most suitable point.
+    *   If it doesnt find a lookahead point, it sets the lookahead to the last point in the path
+    *   @param robotPose current pose of the robot
+    *   @return calculated lookahead of the robot as a Vector2
+    */
     @Suppress("ComplexMethod")
     private fun calculateLookahead(robotPose: Pose2d): Vector2 {
         mLastClosestPointIndex = mPath.findClosestPointIndex(robotPose, mLastClosestPointIndex)
@@ -85,6 +104,13 @@ class PathFollower(path: Path) {
         return lookahead
     }
 
+    /**
+    * calculates curvature between robot point and lookahead point
+    * @param robotPose current pose of the robot
+    * @param lookahead lookahead point
+    * @param robotAngle the modified robot angle to prevent undefined curvatures
+    * @return the curvature of the arc that the robot must follow to reach the lookahead point
+    */
     private fun calculateCurvature(robotPose: Pose2d, lookahead: Vector2, robotAngle: Double): Double {
         val a = (1 / Math.tan(robotAngle))
         val b = -1
@@ -97,15 +123,25 @@ class PathFollower(path: Path) {
         )
         return curvature * side
     }
-
-    fun doneWithPath(robotPos: Pose2d): Boolean {
-        val distance = robotPos.translation.distanceTo(mPath.endPose.translation)
+    /**
+    * @param robotPos position of the robot
+    * @return whether the robot is done with the the designated path based on robot location
+    */
+    fun doneWithPath(robotPose: Pose2d): Boolean {
+        val distance = robotPose.translation.distanceTo(mPath.endPose.translation)
         return distance < Constants.Path.LOOK_AHEAD_DISTANCE
     }
 
+    /**
+    * resets follower to first point
+    */
     fun reset() {
         mLastClosestPointIndex = 0
     }
 
+    /**
+    * @property leftVelocity target velocity of the right side of the drivetrain
+    * @property rightVelocity target velocity of the right side of the drivetrain
+    */
     public data class PathFollowerOutput(val leftVelocity: Double = 0.0, val rightVelocity: Double = 0.0)
 }
