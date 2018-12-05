9765a474317078a4de8b4afb39079818bfecccae
Ben Scholar <bens@digitalman.com>
2018-12-05 15:43:12 -0800

    added tests file

--- src/test/kotlin/tests/path/PathGeneratorTest.kt
+++ src/test/kotlin/tests/path/PathGeneratorTest.kt
@@ -1,12 +1,14 @@
-package tests.utils.path
+package tests.path
 
 import org.junit.jupiter.api.Test
+
+import frc.team5499.frc2018Kotlin.path.PathGenerator
 
 public class PathGeneratorTest {
 
     @Test
     fun test_generator() {
-        // assert(PathGenerator.Paths.startLeftSwitchPath.reversed == true)
+        assert(PathGenerator.Paths.startLeftSwitch.reversed == true)
         // println(PathGenerator.Paths.startLeftSwitchPath.toCSV())
     }
 }
